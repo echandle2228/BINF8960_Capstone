a13a01145945d0cbb82cf8ecd7d111eae1c49789
Emma Chandler <emmakatchandler@gmail.com>
2025-05-29 17:38:36 -0400

    updated setup.sh script to include batch job info

--- setup.sh
+++ setup.sh
@@ -1,11 +1,21 @@
+#!/bin/bash
+#SBATCH --partition=batch
+#SBATCH --job-name=capstone_setup
+#SBATCH --ntasks=1
+#SBATCH --time=2:00:00
+#SBATCH --mem=2gb
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=ec50513@uga.edu
+
+
 # Script to set up analysis of E coli variation for BINF 8960
 
 # Starting in the project folder
 # create the appropriate directory structure
-mkdir -r data/raw_fastq docs results
+mkdir data docs results
 
 # Copy over the raw data files and make them read-only
 cp -r /work/binf8960/instructor_data/raw_fastq ./data/
 
 # Make it read only
-chmod -w data/raw_fastq/*.fastq
+chmod -w data/raw_fastq/*.fastq.gz
